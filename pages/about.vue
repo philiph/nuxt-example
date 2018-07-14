069da9b38c1f4ea7cb1fbe90c5227ef3c4c002a9
Philip Hagen <phil@philiphagen.com>
2018-07-14 22:46:31 +0800

    Add button that reverses some text, on About page

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>About me</h1>
     <p>This page is being rendered as <strong>{{ name }}</strong>.</p>
+
+    <p class="message">{{ message }}</p>
+    <button
+      class="button--grey"
+      @click="reverseMessage">Reverse Message</button>
+
     <nuxt-link
       to="/"
       class="button--grey">Home</nuxt-link>
@@ -15,8 +21,28 @@ export default {
       name: process.static ? 'static' : (process.server ? 'server' : 'client')
     }
   },
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
   head: {
     title: 'About page'
   }
 }
 </script>
+
+<style>
+.message {
+  margin: 20px;
+  padding: 10px;
+  font-size: 1.5em;
+  background-color: #eee;
+  font: consolas;
+}
+</style>
