26cb983c7861b93f5d8dcf4ea949ab0b68339547
Philip Hagen <phil@philiphagen.com>
2018-07-14 15:53:24 +0800

    About page - show whether being rendered as static, server, or client

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <h1>About me</h1>
-    <p>Hello world.</p>
+    <p>This page is being rendered as <strong>{{ name }}</strong>.</p>
     <nuxt-link
       to="/"
       class="button--grey">Home</nuxt-link>
   </div>
 </template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      name: process.static ? 'static' : (process.server ? 'server' : 'client')
+    }
+  }
+}
+</script>
