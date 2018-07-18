86beab80b38fa3b0d3d8c92b863bb70593322876
Philip Hagen <phil@philiphagen.com>
2018-07-18 08:39:11 +0800

    Fetch Bitcoin prices from Coindesk API and display on page

--- pages/bitcoin.vue
+++ pages/bitcoin.vue
@@ -2,7 +2,11 @@
   <div>
     <h1>Bitcoin</h1>
 
-    <p>TODO: Show price here</p>
+    <li
+      v-for="currency in currencies"
+      :key="currency.code">
+      1 BTC = {{ currency.code }} {{ currency.rate }}
+    </li>
   </div>
 </template>
 
@@ -10,6 +14,12 @@
 export default {
   head: {
     title: 'Bitcoin Price'
+  },
+  async asyncData({ app }) {
+    const data = await app.$axios.$get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    const currencies = Object.entries(data.bpi)
+      .map(data => data[1])
+    return { currencies }
   }
 }
 </script>
